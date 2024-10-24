cd0ea1430ac1f43a0e28f69feaae695f8d328016
MarkusIngerslev <markusolsen2004@gmail.com>
2024-10-24 11:49:52 +0200

    Tilføjet kode fra de 4 forskellige video'er så map nu virker med den funktionalitet

--- App.js
+++ App.js
@@ -1,20 +1,97 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from "react-native";
+import { useState, useRef, useEffect } from "react";
+import MapView, { Marker } from "react-native-maps";
+import * as Location from "expo-location";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    const [markers, setMarkers] = useState([]);
+    const [region, setRegion] = useState({
+        latitude: 55,
+        longitude: 12,
+        latitudeDelta: 20,
+        longitudeDelta: 20,
+    });
+
+    const mapView = useRef(null); // ref til mapview objektet
+    const locationSubscription = useRef(null); // når vi lukker appen, skal den ikke lytte mere
+
+    useEffect(() => {
+        async function startListening() {
+            let { status } =
+                await Location.requestForegroundPermissionsAsync();
+            if (status !== "granted") {
+                console.log(
+                    "Permission to access location was denied"
+                );
+                return;
+            }
+            locationSubscription.current =
+                await Location.watchPositionAsync(
+                    {
+                        distanceInterval: 100,
+                        accuracy: Location.Accuracy.High,
+                    },
+                    (lokation) => {
+                        const newRegion = {
+                            latitude: lokation.coords.latitude,
+                            longitude: lokation.coords.longitude,
+                            latitudeDelta: 20,
+                            longitudeDelta: 20,
+                        };
+                        setRegion(newRegion); // Flytter kortet til ny position
+                        if (mapView.current) {
+                            mapView.current.animateToRegion(
+                                newRegion,
+                                1000
+                            );
+                        }
+                    }
+                );
+        }
+        startListening();
+        return () => {
+            if (locationSubscription.current)
+                locationSubscription.current.remove();
+        };
+    }, []);
+
+    function addMarker(data) {
+        const { latitude, longitude } = data.nativeEvent.coordinate;
+        const newMarker = {
+            coordinate: { latitude, longitude },
+            key: data.timeStamp,
+            title: "Great place",
+        };
+        setMarkers([...markers, newMarker]);
+    }
+
+    function onMarkerPress(text) {
+        alert("You pressed a marker with title: " + text);
+    }
+
+    return (
+        <View style={styles.container}>
+            <MapView
+                style={styles.map}
+                region={region}
+                onLongPress={addMarker}
+            >
+                {markers.map((marker) => (
+                    <Marker
+                        coordinate={marker.coordinate}
+                        key={marker.key}
+                        title={marker.title}
+                        onPress={() => onMarkerPress(marker.title)}
+                    />
+                ))}
+            </MapView>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    map: {
+        width: "100%",
+        height: "100%",
+    },
 });
